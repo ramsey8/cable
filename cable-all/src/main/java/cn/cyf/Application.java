e188dee26999edaaab1a4300be3d495341d24d53
cgda123a <cgda123a@aliyun.com>
2019-07-03 23:56:03 +0800

    提交swagger配置

--- cable-all/src/main/java/cn/cyf/Application.java
+++ cable-all/src/main/java/cn/cyf/Application.java
@@ -1,11 +1,16 @@
 package cn.cyf;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = { "cn.cyf" })
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
